baa2b30fc1cd63d6e9334c609da75a57fea343fa
Ben Fiedler <ben@debian.local>
2016-04-15 11:43:25 +0200

    Refactored transform.py

--- lightingsystem/transform.py
+++ lightingsystem/transform.py
@@ -1,5 +1,25 @@
 import datetime, serial
 
+def getLightFromFrequency(data, initdata):
+	frequency = data[0]
+	darkfrequency = initdata[0]
+	temp = data[1]
+
+	frequencydiff = frequency - darkfrequency
+	irradiance = frequencydiff * 100 / 100; # convert to microW/cm^2 and then to W/m^2
+	lightoutput = irradiance * 1^2; # figure distance out (in m)
+	junctiontemp = 80 # figure junction temp out
+	actuallightoutput = lightoutput / ((-0.3 * (junctiontemp - 25) + 100) / 100)
+	return actuallightoutput
+
+def writeTo(f, data):
+	f.write(';'.join(map(str, data)) + '\n')
+
+def readinp():
+	return map(int, ser.readline().split(';'))
+
+# --------- Main begins here ----------- #
+
 ser = serial.Serial(
 	port = 'COM5',\
 	baudrate = 115200)
@@ -23,20 +43,3 @@ while True:
 	rawdata.flush()
 	f.flush()
 
-def getLightFromFrequency(data, initdata):
-	frequency = data[0]
-	darkfrequency = initdata[0]
-	temp = data[1]
-
-	frequencydiff = frequency - darkfrequency
-	irradiance = frequencydiff * 100 / 100; # convert to microW/cm^2 and then to W/m^2
-	lightoutput = irradiance * 1^2; # figure distance out (in m)
-	junctiontemp = 80 # figure junction temp out
-	actuallightoutput = lightoutput / ((-0.3 * (junctiontemp - 25) + 100) / 100)
-	return actuallightoutput
-
-def writeTo(f, data):
-	f.write(';'.join(map(str, data)) + '\n')
-
-def readinp():
-	return map(int, ser.readline().split(';'))
